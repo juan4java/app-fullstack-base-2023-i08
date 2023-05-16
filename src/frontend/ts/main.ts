3a05bca69f2e36736e301d5ba2dc5e977a9565a0
Juan Bampini <juan.c.bampini@gmail.com>
2023-05-15 21:31:31 -0300

    Clase 2

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -4,3 +4,13 @@ function SayHello(){
     let new_value = "Hello world!!!" + "\n" + current_value.value;
     document.getElementById("textarea_1").innerHTML = new_value;
 }
+
+
+var fun:any = SayHello;
+
+window.addEventListener("load", ()=>{
+    
+    fun();
+    console.log("aca2")
+    SayHello();
+});
